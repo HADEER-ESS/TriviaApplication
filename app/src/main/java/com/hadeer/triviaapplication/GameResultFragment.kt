dfd7c7315bf136681e45ea130147b79b14f0dd43
hadeer <hadeeressam21@gmail.com>
2025-07-28 22:36:43 +0300

    $ edt back navigation in result, and handle the View for SUCCESS and FAIL

--- app/src/main/java/com/hadeer/triviaapplication/GameResultFragment.kt
+++ app/src/main/java/com/hadeer/triviaapplication/GameResultFragment.kt
@@ -1,11 +1,14 @@
 package com.hadeer.triviaapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.hadeer.triviaapplication.databinding.FragmentGameResultBinding
 
 
@@ -22,12 +25,30 @@ class GameResultFragment : Fragment() {
         binding = FragmentGameResultBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
         handleBtnNavigation()
+        decideViewResult()
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n", "SuspiciousIndentation")
+    private fun decideViewResult() {
+        val res = arguments?.getInt("result")
+            println("correct answers is $res")
+        if (res != null) {
+            if(res >= 4){
+                binding.wonImg.setImageResource(R.drawable.you_win)
+                binding.actionResultBtn.text = getString(R.string.next_match)
+            } else{
+                binding.wonImg.setImageResource(R.drawable.try_again)
+                binding.actionResultBtn.text = getString(R.string.try_again)
+            }
+        }
+        binding.congratsText.text = " ${getString(R.string.your_score_is)} $res / 4"
+
+    }
+
     private fun handleBtnNavigation() {
-        binding.actionResultBtn.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_gameResultFragment_to_gameFragment)
+        binding.actionResultBtn.setOnClickListener {
+            findNavController().navigateUp()
         }
     }
 }
