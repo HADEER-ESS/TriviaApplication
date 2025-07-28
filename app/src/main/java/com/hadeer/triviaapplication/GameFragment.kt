b4e0910ed4a9631b1e3e7fbb268c2261a468011b
hadeer <hadeeressam21@gmail.com>
2025-07-28 22:39:54 +0300

    $ hadle the count, and select answers for Gam screen

--- app/src/main/java/com/hadeer/triviaapplication/GameFragment.kt
+++ app/src/main/java/com/hadeer/triviaapplication/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +18,7 @@ class GameFragment : Fragment() {
     private lateinit var binding : FragmentGameBinding
     private lateinit var viewModel : QuestionViewModel
     private lateinit var recyclerView : RecyclerView
+    private lateinit var adaptor : AnswersAdaptor
     private var count : Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +42,30 @@ class GameFragment : Fragment() {
 
     private fun handleRenderData() {
         viewModel.currentQuestion.observe(viewLifecycleOwner){question ->
+            println("question is $question")
             binding.textQuestion.text = question.question
-            recyclerView.adapter = AnswersAdaptor(question.answers)
-            count++
-        }
-        binding.submiteBtn.setOnClickListener{
-            if(count < 4){
-                viewModel.goToNextQuestion()
-            }
-            else{
-                findNavController().navigate(R.id.action_gameFragment_to_gameResultFragment)
+            adaptor = AnswersAdaptor(question.answers)
+            recyclerView.adapter = adaptor
+            binding.submiteBtn.setOnClickListener{
+                val selectedAnswer = adaptor.getSelectedAnswer()
+                if(selectedAnswer == null){
+                    Toast.makeText(requireContext(),"Please select answer" , Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+                if(selectedAnswer ==question.correctAns){
+                    viewModel.updateResult()
+                }
+                count++
+
+                if(count == 4){
+                    val argumants = Bundle().apply {
+                        viewModel.currentResult.value?.let { value -> putInt("result", value) }
+                    }
+                    findNavController().navigate(R.id.action_gameFragment_to_gameResultFragment, argumants)
+                }
+                else{
+                    viewModel.goToNextQuestion()
+                }
             }
         }
     }
