738f789fb1c7e4382b296ac65c469210342069fc
hadeer <hadeeressam21@gmail.com>
2025-07-29 21:30:05 +0300

    $ remove the Action BTN for Share apearcne from Main activity to activate it at RESULT SCREEN only

--- app/src/main/java/com/hadeer/triviaapplication/MainActivity.kt
+++ app/src/main/java/com/hadeer/triviaapplication/MainActivity.kt
@@ -105,8 +105,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.app_menu, menu)
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.app_menu, menu)
+//        return true
+//    }
 }
